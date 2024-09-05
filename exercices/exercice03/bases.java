4801f62ea092682a0b144405dbe1b33884c43827
silva-duarte-emf <duarte.silvacorreia@studentfr.ch>
2024-09-05 09:53:20 +0200

    ex1

---
+++ exercices/exercice03/bases.java
@@ -0,0 +1,13 @@
+public class bases {
+
+    public static void main(String[] args) {
+        int age = 15;
+        int agedans10 = 25;
+        String nom = "Duarte";
+
+        System.out.println("Je m'appelle " + nom + " et j'ai " + age + "ans");
+        System.out.println("Dans 10 ans, j'aurais " + agedans10);
+
+    }
+    
+}
