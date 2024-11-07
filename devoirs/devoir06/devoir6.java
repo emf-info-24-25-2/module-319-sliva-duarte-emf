f9b7cb82e4598bd55143686ee02c6c323eb43899
silva-duarte-emf <duarte.silvacorreia@studentfr.ch>
2024-11-07 11:46:20 +0100

    dv6

---
+++ devoirs/devoir06/devoir6.java
@@ -0,0 +1,18 @@
+public class devoir6 {
+
+    public static int[] genereTableau(int min, int max, int taille) {
+
+    final int TAILLE_TABLEAU = 20;
+    final int VALEUR_MIN = 0;
+    final int VALEUR_MAX = 50;
+    final int VALUER_RECHERCHEE = 7;
+
+    int[] tableau = new int [taille];
+    for (int i = 0; i < tableau.length; i++) {
+    tableau[i] = (int) (Math.random() * (VALEUR_MAX - VALEUR_MIN + 1)) + VALEUR_MIN;
+
+    return genereTableau(VALEUR_MIN, VALEUR_MAX, TAILLE_TABLEAU);
+            
+}  
+}
+}
